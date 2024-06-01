8d4404b0652e505ba04ee566c05fdd4aace4b0c8
Juan Jara <131215762+ains1622@users.noreply.github.com>
2024-06-01 18:25:35 -0400

    Las carpetas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Aplicación React</h1>
+      <p>¡Bienvenido!</p>
+    </div>
+  );
+}
+
+export default App; // Exporta el componente App por defecto
