4238da6efa77915f16f958bae9bdc037c7594bd1
Ricardo2dds <ricardododds2garrido@gmail.com>
2024-06-16 15:27:22 -0400

    servlet

--- Java_René/100porcientodeporte/src/main/java/com/mycompany/porcientodeporte/registroServlet.java
+++ Java_René/100porcientodeporte/src/main/java/com/mycompany/porcientodeporte/registroServlet.java
@@ -1,12 +1,13 @@
 package com.mycompany.porcientodeporte;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RegistroServlet extends HttpServlet {
+public class registroServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
